d64beafe448ea7ffd4255039073eac72a0ad8a5b
Bryan Stopp <bryan.stopp@gmail.com>
2015-06-27 14:31:47 -0500

    Adding twitter service API & Impl

---
+++ service-bundle/src/main/java/com/github/bstopp/kcjug/felix/twitter/service/TwitterService.java
@@ -0,0 +1,12 @@
+
+package com.github.bstopp.kcjug.felix.twitter.service;
+
+import com.github.bstopp.kcjug.felix.twitter.Account;
+
+public interface TwitterService {
+
+	String	TWITTER_URL	= "https://twitter.com/";
+	
+	Account getAccount();
+
+}
