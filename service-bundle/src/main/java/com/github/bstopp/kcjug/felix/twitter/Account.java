0465a8a4242ba4111ede7e4571711dbe4c533dea
Bryan Stopp <bryan.stopp@gmail.com>
2015-06-27 14:31:47 -0500

    Adding twitter service API & Impl

---
+++ service-bundle/src/main/java/com/github/bstopp/kcjug/felix/twitter/Account.java
@@ -0,0 +1,69 @@
+
+package com.github.bstopp.kcjug.felix.twitter;
+
+public class Account {
+
+	private Long	userId;
+	private String	screenName;
+	private String	profileUrl;
+	private String	profileImage;
+	private String	description;
+	private int		followers;
+	private int		friends;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getScreenName() {
+		return screenName;
+	}
+
+	public void setScreenName(String screenName) {
+		this.screenName = screenName;
+	}
+
+	public String getProfileUrl() {
+		return profileUrl;
+	}
+
+	public void setProfileUrl(String profileUrl) {
+		this.profileUrl = profileUrl;
+	}
+
+	public String getProfileImage() {
+		return profileImage;
+	}
+
+	public void setProfileImage(String profileImage) {
+		this.profileImage = profileImage;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getFollowers() {
+		return followers;
+	}
+
+	public void setFollowers(int followers) {
+		this.followers = followers;
+	}
+
+	public int getFriends() {
+		return friends;
+	}
+
+	public void setFriends(int friends) {
+		this.friends = friends;
+	}
+}
