94911f82b6ed4009dadd29060abf388945446b69
Bryan Stopp <bryan.stopp@gmail.com>
2015-06-29 08:31:36 -0500

    Moving files to appropriate location and fixing dependencies.

--- service-bundle/src/main/java/com/github/bstopp/kcjug/felix/twitter/service/impl/Twitter4jService.java
+++ service-bundle/src/main/java/com/github/bstopp/kcjug/felix/twitter/service/impl/Twitter4jService.java
@@ -1,23 +1,21 @@
 
 package com.github.bstopp.kcjug.felix.twitter.service.impl;
 
-
 import com.github.bstopp.kcjug.felix.twitter.Account;
 import com.github.bstopp.kcjug.felix.twitter.service.TwitterService;
 
 public class Twitter4jService implements TwitterService {
 
-	
-	@Override
-	public Account getAccount() {
-		Account acct = new Account();
-		acct.setScreenName("@StoppThinking");
-		acct.setDescription("AEM Platform Architect @VML, avid reader, book collector");
-		acct.setFollowers(45);
-		acct.setFriends(44);
-		acct.setProfileUrl(TwitterService.TWITTER_URL + "/StoppThinking");
-		
-		return acct;
-	}
+    @Override
+    public Account getAccount() {
+        Account acct = new Account();
+        acct.setScreenName("@StoppThinking");
+        acct.setDescription("AEM Platform Architect @VML, avid reader, book collector");
+        acct.setFollowers(45);
+        acct.setFriends(44);
+        acct.setProfileUrl(TwitterService.TWITTER_URL + "/StoppThinking");
+
+        return acct;
+    }
 
 }
